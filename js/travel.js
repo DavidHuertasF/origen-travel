aa09ec3d900512d02b1cc795fc58035d72d1b813
David Huertas <david.huertasf@hmotmail.com>
2020-02-17 10:17:40 -0500

    Primera versión: página de inicio, y página de tours cortos por región añadidos

---
+++ js/travel.js
@@ -0,0 +1,51 @@
+var numItems = $('.tp-bullet').length;
+for (var i = 0; i < numItems; i++) {
+        $(".tp-bullet").eq(i).attr('name',i);
+        $(".tp-bullet").eq(i).attr('onmouseover', "stop()");
+        // $(".tp-bullet").eq(i).attr('onmouseout', "restart(this.name)");
+        
+ }
+
+ function start(i){setTimeout(function() { bannerAnimationStart(i);},  2000);};
+ start(0);
+ function restart(i){
+    xx= 0;
+    start(i);
+ }
+
+ var xx = 0;
+ 
+ function bannerAnimationStart(x){
+     var numItems = $('.tp-bullet').length;
+     $(".tp-bullet")[x].click();
+     var o = x+1;
+     for (var i = o; i < numItems+1; i++) {        
+
+             (function(index) {
+                 setTimeout(function() { 
+                     if(xx == 0){
+                         if(index==numItems){
+                             bannerAnimationStart(0);
+                         }
+                         try {
+                         $(".tp-bullet")[index].click(); 
+                             
+                         } catch (error) {
+                             
+                         }
+
+
+                     }
+                     }, i* 7000);
+             })(i);
+
+          
+      }
+ }
+ 
+ 
+ function stop()
+ {
+     xx= 1;
+ }
+
