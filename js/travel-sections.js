80cfe7d163c9e250b9cbc4f7b6910ee68a463244
David Huertas <david.huertasf@hotmail.com>
2020-02-20 17:45:44 -0500

    "se añade imagen y js que no quedaron en el commit anterior

---
+++ js/travel-sections.js
@@ -0,0 +1,14 @@
+$('#sugamuxi_area').hide();
+
+
+$( "#sugamuxi_button" ).click(function() {
+    $('#sugamuxi_area').show();
+  });
+
+  $( "#valle_button" ).click(function() {
+    $('#sugamuxi_area').hide();
+  });
+
+  $( "#norte_button" ).click(function() {
+    $('#sugamuxi_area').hide();
+  });
